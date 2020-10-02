ed0ec1cfbd512719e6e4befd7174cb2c1374cfd2
Alec Wang <alswang18@gmail.com>
2020-10-02 14:19:39 -0700

    added text to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,7 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Welcome to my corner!</h1>
       <h2>I'm Alec Wang, a Computing Scientist excited about data products and open to tech opportunities in North America and Asia-Pacific.</h2>
+      <h3>Most Recent Thoughts:</h3>
       <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge)=>{
           return (
