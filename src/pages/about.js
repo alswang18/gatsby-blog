5369d545bd6a32b6fd105ec3e5880e60b5875982
Alec Wang <alswang18@gmail.com>
2020-10-02 17:29:31 -0700

    added misc text to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const AboutPage = () => {
             <p> Hey I'm Alec!</p>
             <p>I am a data-driven software engineer and problem-solver with experience building data solutions in fast growing companies. 
                 I am passionate about creating data products that improve people's daily lives.</p>
-            <p>I was in <a href="https://www.firstcircle.ph/">First Circle</a> as a Product Intern then transitioned into the Data Team. Before that, I was working in operations or engineering at various B2B SaaS startups: <a href="expedock.com">Expedock</a>, <a href="https://www.exora.ph/">Exora</a>, <a href="https://www.taikuun.com/">Taikuun</a>. I was also a Data Science Bootcamp Associate in <a href="eskwelabs.com">Eskwelabs</a>.</p>
+            <p>I was in <a href="https://www.firstcircle.ph/">First Circle</a> as a Product Intern then transitioned into the Data Team. Before that, I was working in operations or engineering at various B2B SaaS startups: <a href="expedock.com">Expedock</a>, <a href="https://www.exora.ph/">Exora</a>, <a href="https://www.taikuun.com/">Taikuun</a>. I was also employed as a Data Science Bootcamp Associate in <a href="eskwelabs.com">Eskwelabs</a>.</p>
             <p>
                 Currently, I am a student in Simon Fraser University working on my BSC Computing Science degree. 
                 I was a co-organizer of <a href="https://www.facebook.com/groups/effectivealtruismph">Effective Altruism Philippines</a>, the local chapter of the <a href="effectivealtruism.org">effective altruism (EA) movement</a>, and a co-founder of <a href="https://www.facebook.com/eablue.ph">Effective Altruism Blue</a>, an EA student chapter in Ateneo de Manila University.
